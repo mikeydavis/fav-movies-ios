8af82e11d5861c4bba146fda0f180bc4e18a7b5d
Mike Davis <mikedavis@mikes-macbook-air-2.local>
2018-03-29 18:03:48 +0100

    Initial Commit

---
+++ favourite-movies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  favourite-movies
+//
+//  Created by Mike Davis on 29/03/2018.
+//  Copyright © 2018 Midax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
